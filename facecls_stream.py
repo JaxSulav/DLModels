d955f4d8f15d39bf47f4880fd2b43a997b9a8d09
Sulav Shrestha <stha.sulabh@gmail.com>
2024-04-09 16:47:48 -0400

    Remove display of uploaded image again

--- facecls_stream.py
+++ facecls_stream.py
@@ -40,7 +40,6 @@ st.title("Facial Expression Recognition")
 uploaded_file = st.file_uploader("Upload an image...", type=['jpg', 'png', 'jpeg'])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
     
     if st.button('Predict'):
         expression_id, probabilities = predict_expression(image)
